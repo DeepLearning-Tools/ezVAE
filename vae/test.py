f7bc4047d5c842e6d3c40ac136f469d91e547f6b
alexandreday <alexandre.day1@gmail.com>
2018-02-13 13:51:16 -0500

    vae class basics

---
+++ vae/test.py
@@ -0,0 +1,32 @@
+# THINGS TO try and TEST NOW:
+# -------------------------------------------
+# 1. VAE ARCHITECTURE
+# 2. SIZE OF TRAINED DATASET
+# 3. GAUSSIAN MIXTURES VS MNIST ETC.
+# -------------------------------------------
+
+def main():
+
+    root = '/Users/alexandreday/Desktop/clustering-references/analysis/'
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.astype('float32') / 255.
+    x_test = x_test.astype('float32') / 255.
+    x_train=np.reshape(x_train, (len(x_train), 784))
+    x_test=np.reshape(x_test, (len(x_test), 784))
+
+    name = 'noiseVAE_noise0p2.h5'
+    fname = root+name # for loading vae model in case u need it
+    #test(y_train[:10000])
+    #exit()
+    VAE = load_model(fname) 
+    #VAE = train_model(x_train[:10000], x_test, root=root, name=name, drop_rate = 0.2, epoch = 500)
+    #exit()
+    xvae = VAE.predict(x_train[:10000])
+    pickle.dump(xvae, open('xvae_test.pkl','wb'))
+    
+    exit()
+    #xpca = PCA(n_components=80).fit_transform(xvae)
+    xtsne = tsne_model(x_train[:10000], savefile = 'test.pkl', n_iter = 5000)
+    #xtsne = tsne_model(xvae, savefile = 'tsne_drop0p2_angle0p5.pkl', n_iter = 5000)
+    plot_w_label(xtsne, y_train[:10000])
---
+++ vae/vae.py
@@ -0,0 +1,56 @@
+from keras.layers import Input, Dense, Dropout
+from keras.models import Model
+import numpy as np
+from keras import backend as K
+from keras.models import load_model as lmm
+import tensorflow as tf
+
+class VAE:
+    
+    def __init__(self, name='tmp.h5', drop_rate = 0.7, epoch=50, dense_size=1000, batch_size=256):
+        self.name = name
+        self.drop_rate = drop_rate
+        self.epoch = epoch
+        self.dense_size = dense_size
+        self.batch_size = batch_size
+        self.root = ""
+
+    def train(self, x_train, x_test):
+        self.input_dimension = x_train.shape[1]
+
+        ### -> building network
+        input_img = Input(shape=(self.input_dimension,))  # adapt this if using `channels_first` image data format
+        x = Dropout(self.drop_rate)(input_img)
+        encoded = Dense(self.dense_size, activation='relu', name='encoder')(x) # need to save weigths and biases of this
+        x = Dropout(self.drop_rate)(encoded)
+        decoded = Dense(self.input_dimension, activation='relu')(x)
+
+        autoencoder = Model(input_img, decoded)
+        autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy') # here pixels are set between 0 and 1.
+
+        #autoencoder.compile(optimizer='adadelta', loss='mean_squared_error')
+
+        autoencoder.fit(x_train, x_train,
+                        epochs = self.epoch,
+                        batch_size=self.batch_size,
+                        shuffle=True,
+                        validation_data=(x_test, x_test)
+        )
+
+        # getting rid of decoding and noising layers, return only the dense layer.
+        layer = autoencoder.get_layer(name='encoder')
+        input_img = Input(shape=(self.input_dimension,))
+        encoder = Dense(self.dense_size, activation='relu', name='first')(input_img)
+        encoder_model = Model(input=input_img, output=encoder)
+        encoder_model.set_weights([layer.get_weights()[0]*(1-self.drop_rate),layer.get_weights()[1]]) # rescaling weights here
+        encoder_model.save(self.root+self.name) # use encoder_model.predict(x) to output a 1000 dim representation
+        self.encoder_model = encoder_model
+
+    def predict(self, x):
+        return self.encoder_model.predict(x)
+
+    def load_model(self, name=None):
+        if name is None:
+            self.encoder_model = lmm(self.name)
+        else:
+            self.encoder_model = lmm(name)
